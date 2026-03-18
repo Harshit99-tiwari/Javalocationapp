7186cfb40270ce9c3f3825d1afc708bb08368800
Harshit99-tiwari <tiwariankush656@gmail.com>
2026-03-18 23:42:02 +0530

    Add location feature with permissions and emulator support

--- app/src/main/java/com/example/javalocationapp/MainActivity.java
+++ app/src/main/java/com/example/javalocationapp/MainActivity.java
@@ -1,24 +1,58 @@
 package com.example.javalocationapp;
 
-import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import androidx.core.app.ActivityCompat;
+
+import android.location.Location;
+import android.location.LocationManager;
+import android.content.Context;
+
+import android.location.Geocoder;
+import java.util.List;
+import android.location.Address;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView locationText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        locationText = findViewById(R.id.locationText);
+    }
+
+    public void getLocation(View view) {
+
+        LocationManager locationManager =
+                (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        if (ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+            return;
+        }
+
+        Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+        if (location != null) {
+            double lat = location.getLatitude();
+            double lon = location.getLongitude();
+
+            locationText.setText("Lat: " + lat + "\nLon: " + lon);
+        } else {
+            locationText.setText("Turn ON location and try again");
+        }
     }
 }
