d983f6700a984b822f37bbc49aecc2a920ac3b36
Harshit99-tiwari <tiwariankush656@gmail.com>
2026-03-22 18:43:38 +0530

    Add LocationModel class for RecyclerView data

---
+++ app/src/main/java/com/example/javalocationapp/locationmodel.java
@@ -0,0 +1,14 @@
+package com.example.javalocationapp;
+
+public class locationmodel {
+
+    double latitude;
+    double longitude;
+    String address;
+
+    public locationmodel(double latitude, double longitude, String address) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.address = address;
+    }
+}
